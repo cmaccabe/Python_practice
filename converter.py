5119bfd888771f58d23e31e5754a32c3e8753887
Shady <105404441+cmaccabe@users.noreply.github.com>
2024-07-06 13:57:04 -0500

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,7 @@
+print("How many kilometers did you cycle today?")
+kms = float(input())
+miles = kms/1.60934
+miles = round(miles, 2)
+print(f"That is equal to {miles} miles ")
+
+#converts kilometers to miles, rounds the answer to 2 decimals
